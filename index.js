881ee6cd94a21751338dc16ef1cb54b5bfa75a28
OrangeBlue <snackcandy105@gmail.com>
2026-02-27 18:36:41 +0900

    백엔드 URL 형식 수정

--- index.js
+++ index.js
@@ -320,21 +320,21 @@ client.setMaxListeners(15);
 const monitorZones = [
     {
         name: "독도 태극기",
-        tileUrl: "https://backend.wplace.live/files/s0/tiles/1774/795.png",
+        tileUrl: "https://backend.wplace.live/tile/1774/795.png",
         originalPath: "./assets/ref_dokdo.png",
         x: 40, y: 100, width: 361, height: 261,
         wplaceUrl: "https://wplace.live/?lat=37.26901731348799&lng=131.8750484967773&zoom=12.532754498466533"
     },
     {
         name: "서울 태극기",
-        tileUrl: "https://backend.wplace.live/files/s0/tiles/1746/793.png",
+        tileUrl: "https://backend.wplace.live/tile/1746/793.png",
         originalPath: "./assets/ref_seoul.png",
         x: 420, y: 691, width: 160, height: 120,
         wplaceUrl: "https://wplace.live/?lat=37.47464909223321&lng=127.00204068427732&zoom=13.744052003011783"
     },
     {
         name: "백두산 태극기",
-        tileUrl: "https://backend.wplace.live/files/s0/tiles/1752/760.png",
+        tileUrl: "https://backend.wplace.live/tile/1752/760.png",
         originalPath: "./assets/ref_baekdu.png",
         x: 374, y: 111, width: 204, height: 159,
         wplaceUrl: "https://wplace.live/?lat=42.00718311351218&lng=128.05373990302732&zoom=13.726827756600123"
@@ -529,7 +529,7 @@ async function finalizeRecord(userId) {
 
             const compressedOutputPath = path.join(tmpDir, 'output_compressed.mp4');
 
-            // CRF 값을 32로 높여서(화질 감소, 용량 대폭 감소) 재압축
+            // CRF 값을 26으로 높여서(화질 감소, 용량 대폭 감소) 재압축
             await new Promise((resolve, reject) => {
                 const reEncode = spawn('ffmpeg', [
                     '-i', outputPath,
