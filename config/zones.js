31f6abd6141b58dba711759918dba44f4e09a0af
OrangeBlue2336 <snackcandy105@gmail.com>
2026-09-01 02:35:54 +0900

    리팩토링 4단계 완료 - 감시 구역 설정 분리

---
+++ config/zones.js
@@ -0,0 +1,25 @@
+const monitorZones = [
+    {
+        name: "독도 태극기",
+        tileUrl: "https://backend.wplace.live/files/s0/tiles/1774/795.png",
+        originalPath: "./assets/ref_dokdo.png",
+        x: 40, y: 100, width: 361, height: 261,
+        wplaceUrl: "https://wplace.live/?lat=37.26901731348799&lng=131.8750484967773&zoom=12.532754498466533"
+    },
+    {
+        name: "서울 태극기",
+        tileUrl: "https://backend.wplace.live/files/s0/tiles/1746/793.png",
+        originalPath: "./assets/ref_seoul.png",
+        x: 420, y: 691, width: 160, height: 120,
+        wplaceUrl: "https://wplace.live/?lat=37.47464909223321&lng=127.00204068427732&zoom=13.744052003011783"
+    },
+    {
+        name: "백두산 태극기",
+        tileUrl: "https://backend.wplace.live/files/s0/tiles/1752/760.png",
+        originalPath: "./assets/ref_baekdu.png",
+        x: 367, y: 38, width: 238, height: 338,
+        wplaceUrl: "https://wplace.live/?lat=42.00718311351218&lng=128.05373990302732&zoom=13.726827756600123"
+    }
+];
+
+module.exports = monitorZones;
